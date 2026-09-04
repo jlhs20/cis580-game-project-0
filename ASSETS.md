4a13b65f03511af7cea173babe2c32fc2914641a
johnh <johnh@JOHN-VICTUS>
2026-09-04 15:34:45 -0500

    added animation film strip

--- ASSETS.md
+++ ASSETS.md
@@ -1,3 +1,5 @@
 cbg-crop.png CC-BY MazeNL77 https://commons.wikimedia.org/wiki/File:Cute_Ball_-_Games.png cropped in Paint.NET
 
 noiseTexture-1024-4.png own work, made using https://gen3vra.github.io/perlinnoisegenerator/ and Audacity wahwah filter
+
+piscesfilm.png own work, made with Paint.NET, pygame, windows screenshot tool, and imagemagick
--- Game1.cs
+++ Game1.cs
@@ -13,6 +13,9 @@ public class Game1 : Game
     private MathHelper.Random _random = new();
     private Texture2D _background;
     private SpriteFont _font;
+    private Texture2D _pisces;
+    private int _animationFrame = 0;
+    private double _animationTimer = 0.0f;
 
     public Game1()
     {
@@ -45,6 +48,7 @@ public class Game1 : Game
 
         _background = Content.Load<Texture2D>("noiseTexture-1024-4");
         _font = Content.Load<SpriteFont>("arial");
+        _pisces = Content.Load<Texture2D>("piscesfilm");
     }
 
     protected override void Update(GameTime gameTime)
@@ -56,21 +60,34 @@ public class Game1 : Game
         {
             cbg.Update(gameTime);
         }
+        _animationTimer += gameTime.ElapsedGameTime.TotalSeconds;
+        if (_animationTimer > 0.07f) 
+        {
+            _animationFrame = (_animationFrame + 1)%36;
+            _animationTimer -= 0.07f;
+        }
 
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        //GraphicsDevice.Clear(Color.Green);
 
         _spriteBatch.Begin();
-        _spriteBatch.Draw(_background, new Vector2(0, 0), null, Color.CornflowerBlue);
+        for (int x = 0; x < GraphicsDevice.Viewport.Width; x += 300)
+        {
+            for (int y = 0; y < GraphicsDevice.Viewport.Height; y += 300)
+            {
+                _spriteBatch.Draw(_pisces, new Vector2(x, y), new Rectangle(((_animationFrame+x+y)%36)*300, 0, 300, 300), Color.White);
+            }
+        }
+        _spriteBatch.Draw(_background, new Vector2(0, 0), null, new Color(63, 63, 63, 63));
         foreach (CuteBallGames cbg in _cbgs)
         {
             cbg.Draw(gameTime, _spriteBatch);
         }
-        _spriteBatch.DrawString(_font, "Exit game with ESC", new Vector2(0, 0), Color.Gold);
+        _spriteBatch.DrawString(_font, "Exit game with ESC", new Vector2(0, 0), Color.Black);
         _spriteBatch.End();
 
         base.Draw(gameTime);
